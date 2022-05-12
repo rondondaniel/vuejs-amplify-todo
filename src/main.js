3f9adb4a20323672ea924c16f5cfe7c2a3e3fb94
Daniel Rondon <rondondaniel@gmail.com>
2022-05-12 20:53:51 +0200

    Update Amplify config

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
 
 Vue.config.productionTip = false
 
